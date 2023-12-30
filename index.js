a55a6597ac613362ec909ab2cc1f8534f0d7a66e
Nihat Mammadli <nihatmammadli42@gmail.com>
2023-12-30 10:39:02 +0400

    push to the top without smooth effect

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-document.addEventListener('click', e=> {
-  console.log(e.clientX);
-})
+const arrow = document.querySelector(".arrow");
+
+
+arrow.addEventListener('click', function() {
+  // Scroll to the top of the page
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Optional: Adds smooth scrolling animation
+      
+  });
+
+  arrow.animate({ 
+    transform: scrollTo
+  }, {
+    duration: 4000,
+    easing:"ease"
+  })
+
+});
